90ce40cae686d8fcf38d3ac879b11a8025cd3658
Himanshu Gupta <himanshuz@google.com>
2026-02-11 14:17:07 +0000

    Fix side channel attacks in CP2
    Currently, CP2 does not impose any
    strict sql checks to callers allowing callers
    to use malformed queries to perform side channel
    attacks by parsing the return trings from the SQLite
    exception thrown.
    
    This change fixes the same by stripping SQLite error
    messages when text containing "json" is present in same.
    This is only done for callers without "READ_CONTACTS" as
    callers with the same can anyways read the entrie cp2 db.
    
    Bug: 465133716
    Flag: EXEMPT CVE_FIX
    Test: atest ContactsProviderTests
    Change-Id: Ica3d495519430f846247e6c9d62bd572f8dc62bc
    (cherry picked from commit 6044ca5db93f9e8e5ef59dd94b97b7039a3e2c7d)

--- src/com/android/providers/contacts/ContactsProvider2.java
+++ src/com/android/providers/contacts/ContactsProvider2.java
@@ -64,6 +64,7 @@ import android.database.MatrixCursor.RowBuilder;
 import android.database.MergeCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDoneException;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -2365,6 +2366,26 @@ public class ContactsProvider2 extends AbstractContactsProvider
         mInProfileMode.set(false);
     }
 
+    private void maybeStripAndThrowSQLiteExceptionWithJson(Exception e, String permission,
+            LogFields.Builder logBuilder) {
+        // b/465133716: Using certain "json" tokens exposes a side channel attack by deciphering
+        // the error message, so we strip the same.
+        //
+        // Note: We use checkCallingPermission() instead of checkCallingOrSelfPermission()
+        // intentionally. If the call is coming from a non-binder thread (e.g., self-call
+        // after clearing identity), we don't want to grant access to the raw error message
+        // if it might contain sensitive info triggered by user-provided SQL.
+        if (e instanceof SQLiteException
+                && getContext().checkCallingPermission(permission) != PERMISSION_GRANTED) {
+            final String message = e.getMessage();
+            if (message != null && message.toLowerCase(Locale.ROOT).contains("json")) {
+                SQLiteException strippedEx = new SQLiteException("Stripped exception message");
+                logBuilder.setException(strippedEx);
+                throw strippedEx;
+            }
+        }
+    }
+
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         LogFields.Builder logBuilder = LogFields.Builder.aLogFields()
@@ -2396,6 +2417,7 @@ public class ContactsProvider2 extends AbstractContactsProvider
             resultUri = super.insert(uri, values);
             return resultUri;
         } catch (Exception e) {
+            maybeStripAndThrowSQLiteExceptionWithJson(e, WRITE_PERMISSION, logBuilder);
             logBuilder.setException(e);
             throw e;
         } finally {
@@ -2437,6 +2459,7 @@ public class ContactsProvider2 extends AbstractContactsProvider
             updates = super.update(uri, values, selection, selectionArgs);
             return updates;
         } catch (Exception e) {
+            maybeStripAndThrowSQLiteExceptionWithJson(e, WRITE_PERMISSION, logBuilder);
             logBuilder.setException(e);
             throw e;
         } finally {
@@ -2475,6 +2498,7 @@ public class ContactsProvider2 extends AbstractContactsProvider
             deletes = super.delete(uri, selection, selectionArgs);
             return deletes;
         } catch (Exception e) {
+            maybeStripAndThrowSQLiteExceptionWithJson(e, WRITE_PERMISSION, logBuilder);
             logBuilder.setException(e);
             throw e;
         } finally {
@@ -5678,13 +5702,18 @@ public class ContactsProvider2 extends AbstractContactsProvider
         try {
             cursor = queryInternal(uri, projection, selection, selectionArgs, sortOrder,
                     cancellationSignal);
+            if (cursor != null) {
+                // Consuming the cursor allows the actual exception (if any) thrown to be
+                // caught in the exception block below.
+                logBuilder.setResultCount(cursor.getCount());
+            }
             return cursor;
         } catch (Exception e) {
+            maybeStripAndThrowSQLiteExceptionWithJson(e, READ_PERMISSION, logBuilder);
             logBuilder.setException(e);
             throw e;
         } finally {
-            LogUtils.log(
-                    logBuilder.setResultCount(cursor == null ? 0 : cursor.getCount()).build());
+            LogUtils.log(logBuilder.build());
         }
     }
 
